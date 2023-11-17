965e6fec788cb010cb5b11a4f7c3ebf23b5bdb2e
Kieran Hillman <developedbykieran@gmail.com>
2023-11-17 08:20:43 +0000

    Add new about app, add installed app to settings

---
+++ about/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class About(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"About: {self.title} last updated: {self.updated_on}"
