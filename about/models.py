640ab5223b3e7e814485bcfcb3be78292ca5ef81
Kieran Hillman <developedbykieran@gmail.com>
2023-11-20 14:54:46 +0000

    Allow users to leave a collaboration request via a form on about.html

--- about/models.py
+++ about/models.py
@@ -4,9 +4,19 @@ from django.db import models
 
 
 class About(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
     def __str__(self):
-        return f"About: {self.title} last updated: {self.updated_on}"
+        return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
